698dc46a8f8899528851534917adc645dc28607c
JeremyCrichton <22128243+JeremyCrichton@users.noreply.github.com>
2024-05-05 16:12:00 +0100

    setup db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/9860a148-1164-4318-a821-c4935d37ef1a-1xedu7.png",
@@ -9,7 +10,10 @@ const mockUrls = [
 
 const mockImages = mockUrls.map((url, index) => ({ id: index + 1, url }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log("posts", posts); // running only on the server
+
   return (
     <main>
       <div className="flex flex-wrap gap-4">
